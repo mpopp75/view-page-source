ecd17d6905ef7291a2ea9029c812a423facb264d
Markus Popp <git@mpopp.net>
2015-08-18 03:40:03 +0200

    Various improvements ...
    * no Page source for about: pages
    * open source tab to the right of active tab

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var tabs = require("sdk/tabs");
 
 var button = buttons.ActionButton({
   id: "viewsource",
-  label: "View Source",
+  label: "View Page Source",
   icon: {
     "16": "./view-source-16.png",
     "32": "./view-source-32.png",
@@ -13,5 +13,18 @@ var button = buttons.ActionButton({
 });
 
 function viewSource(state) {
-  tabs.open("view-source:" + tabs.activeTab.url);
+  var activeTabIndex = tabs.activeTab.index;
+
+  // no page source if already page source or for about: pages
+  if (tabs.activeTab.url.substring(0,12) != "view-source:" &&
+      tabs.activeTab.url.substring(0,6)  != "about:") {
+    tabs.open({
+      url: "view-source:" + tabs.activeTab.url,
+      onOpen: function onOpen(tab) {
+        // open view source tab to the right of active tab
+        tab.index = activeTabIndex + 1;
+      }
+    });
+
+  }
 }
