a8df9348eb46816ed3e9cac3b211467cb6a22275
Markus Popp <git@mpopp.net>
2015-08-19 04:51:13 +0200

    #1: Make button inactive if no source to be shown

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
+var tch = require("./tabchanged");
+var aps = require("./allowpagesource");
+
+var buttonsEnabled = {
+  "16": "./view-page-source-16.png",
+  "32": "./view-page-source-32.png",
+  "64": "./view-page-source-64.png"
+};
+
+var buttonsDisabled = {
+  "16": "./view-page-source-inactive-16.png",
+  "32": "./view-page-source-inactive-32.png",
+  "64": "./view-page-source-inactive-64.png"
+};
 
 var button = buttons.ActionButton({
   id: "viewpagesource",
   label: "View Page Source",
-  icon: {
-    "16": "./view-page-source-16.png",
-    "32": "./view-page-source-32.png",
-    "64": "./view-page-source-64.png"
-  },
+  icon: buttonsEnabled,
   onClick: viewPageSource
 });
 
-function viewPageSource(state) {
+function viewPageSource() {
   var activeTabIndex = tabs.activeTab.index;
 
-  // no page source if already page source or for about: pages
-  if (tabs.activeTab.url.substring(0,12) != "view-source:" &&
-      tabs.activeTab.url.substring(0,6)  != "about:") {
-    tabs.open({
-      url: "view-source:" + tabs.activeTab.url,
-      onOpen: function onOpen(tab) {
-        // open view source tab to the right of active tab
-        tab.index = activeTabIndex + 1;
-      }
-    });
-
-  }
+  tabs.open({
+    url: "view-source:" + tabs.activeTab.url,
+    onOpen: function onOpen(tab) {
+      // open view source tab to the right of active tab
+      tab.index = activeTabIndex + 1;
+    }
+  });
 }
+
+tch.TabChanged(tabs, button, buttonsEnabled, buttonsDisabled);
